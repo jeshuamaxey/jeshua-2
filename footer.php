b4b2012cba1eb67ee94ac0066b2d63b239784f25
Jeshua Maxey <jeshuamaxey@gmail.com>
2013-05-24 00:26:51 +0100

    redesigned footer. More elegant now

--- footer.php
+++ footer.php
@@ -14,9 +14,9 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<?php do_action( 'jeshua_2_0_credits' ); ?>
-			<span class="footer-credits">Helped along a little by <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'jeshua_2_0' ); ?>" rel="generator"><?php printf( __( '%s', 'jeshua_2_0' ), 'WordPress' ); ?></a></span>
+			<span class="footer-credits">Thanks: <a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'jeshua_2_0' ); ?>" rel="generator"><?php printf( __( '%s', 'jeshua_2_0' ), 'WordPress' ); ?></a></span>
 			<!--<span class="sep"> | </span>-->
-			<span class="footer-credits"><?php printf( __( 'Theme: %1$s by %2$s.', 'jeshua_2_0' ), 'jeshua-2.0', '<a href="http://jeshua.co/" rel="designer">Jeshua Maxey</a>' ); ?></span>
+			<span class="footer-credits"><?php printf( __( 'Theme: %1$s', 'jeshua_2_0' ), '<a href="https://github.com/jeshuamaxey/jeshua-2.0.git" rel="designer">Jeshua 2.0</a>' ); ?></span>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 	<div class='divide footer-divide'><span class='divide-char'>&sect;</span></div>
